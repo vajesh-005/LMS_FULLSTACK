efd7f086666cef9654b0950dcd8afc75e87da32c
Vajesh babu Haribabu <vajeshbh@lumel.com>
2025-06-13 18:10:16 +0530

    port change commit

--- server/server.js
+++ server/server.js
@@ -1,26 +1,27 @@
-require('dotenv').config();
-const hapi = require('@hapi/hapi');
-const { user } = require('./plugins/user');
-const { leave } = require('./plugins/leave');
+require("dotenv").config();
+const hapi = require("@hapi/hapi");
+const { user } = require("./plugins/user");
+const { leave } = require("./plugins/leave");
 require("./cron_job/cron");
 
 async function serverInit() {
-    const server = hapi.server({
-        port: 1110,
-        host: 'localhost', 
-        routes: {
-            cors: {
-                origin: ["http://localhost:5173"],
-                credentials: true
-            }
-        }
-    });
-    
-    await server.register(user);
-    await server.register(leave);
-    await server.start();
+  const server = hapi.server({
+    port: process.env.PORT || 1110,
+    host: "0.0.0.0",
 
-    console.log('Server started at:', server.info.uri);
+    routes: {
+      cors: {
+        origin: ["http://localhost:5173"],
+        credentials: true,
+      },
+    },
+  });
+
+  await server.register(user);
+  await server.register(leave);
+  await server.start();
+
+  console.log("Server started at:", server.info.uri);
 }
 
 serverInit();
